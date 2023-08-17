9cb8056d6992a19f81f746ea6d99643833bda803
Cyril Laferriere <claferri@bess-f1r5s6.clusters.42paris.fr>
2023-08-17 19:23:00 +0200

    norminette

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,18 +6,18 @@
 /*   By: claferri <claferri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 13:38:34 by claferri          #+#    #+#             */
-/*   Updated: 2023/08/17 09:33:04 by claferri         ###   ########.fr       */
+/*   Updated: 2023/08/17 19:22:35 by claferri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_POINT_H
+#ifndef FT_H
 
-#define FT_POINT_H
+# define FT_H
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
